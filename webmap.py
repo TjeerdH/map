1c0f42cdd07fd9d0caafd2adf23bdfb316beba87
TjeerdH <tjeerd.hoekstra@icloud.com>
2021-02-09 15:04:39 +0100

    added layer control

--- webmap.py
+++ webmap.py
@@ -19,13 +19,14 @@ def color_function(name):
 
 map = folium.Map(location=[52.46564496296631, 6.333438527047803], tiles='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', attr='"https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>')
 #map.add_child(folium.Marker(location=[52.46564496296631, 6.333438527047803],popup="My home airport!", icon=folium.Icon(color="red")))
-
+layer_control = folium.LayerControl()
 airports = folium.FeatureGroup(name="Airports")
 
 for lt, ln, n, e in zip(lat,lon, name, elevation):
 	airports.add_child(folium.Marker(location=[lt, ln], popup=f"Name:{n}\n Elevation:{e} ft ", icon=folium.Icon(color=color_function(n))))
 
 map.add_child(airports)
+map.add_child(layer_control)
 
 
 map.save("map.html")
